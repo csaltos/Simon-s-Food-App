5284bcb2a603eadd0f31d52b2c91b3b33b25d5ac
Simon Cross <sicross@gmail.com>
2012-03-01 13:56:22 +0000

    able to publish actions from the pages

--- app.rb
+++ app.rb
@@ -6,14 +6,6 @@ enable :sessions
 set :raise_errors, false
 set :show_exceptions, false
 
-# Scope defines what permissions that we are asking the user to grant.
-# In this example, we are asking for the ability to publish stories
-# about using the app, access to what the user likes, and to be able
-# to use their pictures.  You should rewrite this scope with whatever
-# permissions your app needs.
-# See https://developers.facebook.com/docs/reference/api/permissions/
-# for a full list of permissions
-FACEBOOK_SCOPE = 'user_likes,user_photos,user_photo_video_tags'
 
 unless ENV["FACEBOOK_APP_ID"] && ENV["FACEBOOK_SECRET"]
   abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
@@ -26,29 +18,13 @@ before do
   end
 end
 
-helpers do
-  def url(path)
-    base = "#{request.scheme}://#{request.env['HTTP_HOST']}"
-    base + path
-  end
 
-  def post_to_wall_url
-    "https://www.facebook.com/dialog/feed?redirect_uri=#{url("/close")}&display=popup&app_id=#{@app.id}";
-  end
 
-  def send_to_friends_url
-    "https://www.facebook.com/dialog/send?redirect_uri=#{url("/close")}&display=popup&app_id=#{@app.id}&link=#{url('/')}";
-  end
 
-  def authenticator
-    @authenticator ||= Mogli::Authenticator.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], url("/auth/facebook/callback"))
-  end
-
-  def first_column(item, collection)
-    return ' class="first-column"' if collection.index(item)%4 == 0
-  end
+get "/channel" do
+  script = "<script src=\"//connect.facebook.net/en_US/all.js\"></script>"
+  erb script, :layout => false
 end
-
 
 
 get "/" do
